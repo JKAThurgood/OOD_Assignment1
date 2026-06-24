adb0cd38898ec239d2db88fb45694e5962391051
Jake Thurgood <jkathurgood@gmail.com>
2026-06-23 19:05:52 -0600

    Added Point

---
+++ app/src/main/kotlin/org/example/Point.kt
@@ -0,0 +1,14 @@
+package org.example
+
+class Point(private var x: Double, private var y: Double) : Shape() {
+
+    fun getX(): Double = x
+    fun getY(): Double = y
+
+    fun clone(): Point = Point(x, y)
+
+    override fun move(deltaX: Double, deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
